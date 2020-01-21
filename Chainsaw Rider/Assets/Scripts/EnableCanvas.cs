92a3e978dd9dd4346c6a7769276ba21b64a2675f
DanielPinnington <danielpinnington12@gmail.com>
2020-01-21 17:13:13 +0000

    rip

---
+++ Chainsaw Rider/Assets/Scripts/EnableCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCanvas : MonoBehaviour
+{
+    public GameObject canvas;
+
+    public void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    public void canvasActivated()
+    {
+        canvas.SetActive(true);
+    }
+}
---
+++ Chainsaw Rider/Assets/Scripts/FinishingLineScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishingLineScript : MonoBehaviour
+{
+    public GameObject canvas;
+    public bool pause;
+
+    [SerializeField] private string loadLevel;
+
+
+    private float pauseUntil;
+    private bool paused;
+
+    void Update()
+    {
+        if (paused) CheckPause();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            canvas.SetActive(true);
+            PauseForDuration(1.0f);
+        }
+    }
+
+    void PauseForDuration(float duration)
+    {
+        paused = true;
+        pauseUntil = Time.realtimeSinceStartup + duration;
+        Time.timeScale = 0.0f;
+    }
+
+    void CheckPause()
+    {
+        if (Time.realtimeSinceStartup >= pauseUntil)
+        {
+            Time.timeScale = 1.0f;
+            paused = false;
+            SceneManager.LoadScene("YouWin");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
