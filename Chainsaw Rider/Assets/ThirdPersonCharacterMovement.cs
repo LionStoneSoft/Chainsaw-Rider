6354e8cffe8195d5441f4f5962e7ad8090efb193
Lion Stone <44401494+LionStoneSoft@users.noreply.github.com>
2020-01-21 16:29:20 +0000

    Better movement

--- Chainsaw Rider/Assets/ThirdPersonCharacterMovement.cs
+++ Chainsaw Rider/Assets/ThirdPersonCharacterMovement.cs
@@ -2,20 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ThirdPersonCharacterMovement : MonoBehaviour
 {
     public float Speed;
+    public float TurnSpeed;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
 
     void Update()
     {
         PlayerMovement();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 
     void PlayerMovement()
     {
-        //float hor = Input.GetAxis("Horizontal");
+        float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
-        Vector3 playerMovement = new Vector3(0, 0f, ver) * Speed * Time.deltaTime;
-        transform.Translate(playerMovement, Space.Self);
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed * ver);
+        transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * hor);
     }
+
+
 }
