7bb11886d317ad4e4cdfe533c00c05bf987734e5
DanielPinnington <danielpinnington12@gmail.com>
2020-01-29 23:06:29 +0000

    Respawning
    Respawning script has been implemented, an Array of Transform objects (waypoints/spawn locations) will be added to the game scene (empty game objects). A loop loops over the closest game object positionings to the Player object and chooses the closest possible position.
    
    Possible bugs in future: When player is destroyed it MAY instantiate a deleted player object.

---
+++ Chainsaw Rider/Assets/Scripts/Respawning.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawning : MonoBehaviour
+{
+
+    public Transform[] safePoints;
+    Transform closestTransform;
+
+    void Update()
+    {
+        // You have to set up an input button in the Input manager for this!
+        if (Input.GetButtonDown("reset"))
+        {
+            // put this in a different function for general cleanliness
+            ResetCar();
+        }
+    }
+    void ResetCar()
+    {
+        // first, find the closest safe place
+        float closestDistance = 9999999999;
+        Vector3 currentPos = transform.position;
+        // This goes through every possible safe place and picks the best one
+        foreach (Transform trans in safePoints)
+        {
+            float currentDistance = Vector3.Distance(currentPos, trans.position);
+            if (currentDistance < closestDistance)
+            {
+                closestDistance = currentDistance;
+                closestTransform = trans;
+            }
+        }
+
+        // Now we reset the car!
+        transform.position = closestTransform.position;
+        transform.rotation = closestTransform.rotation;
+    }
+}
