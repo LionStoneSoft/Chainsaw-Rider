1d3a40f8b02f4e4114d440cbee40259a1f3ea599
DanielPinnington <danielpinnington12@gmail.com>
2020-02-10 18:30:20 +0000

    Missile Tracking Player
    Missile script implemented which will follow the players transform, this is simple code just to get things working which it is.
    
    Bugs so far: Missile clipping through floor (when player is beneath ground).
    Missile spins around the player (can be fixed by destroying itself on collision)
    
    Want to be implemented later: Ability to at run-time give the player the ability to fire at targets that are only infront of the player, the object is hard-implemented atm.

---
+++ Chainsaw Rider/Assets/Scripts/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public Transform rocketTarget;
+    public Rigidbody rocketRigidbody;
+
+    public float turn;
+    public float rocketVelocity;
+
+    private void FixedUpdate()
+    {
+        rocketRigidbody.velocity = transform.forward * rocketVelocity;
+
+        var rocketTargetRotation = Quaternion.LookRotation(rocketTarget.position - transform.position);
+
+        rocketRigidbody.MoveRotation(Quaternion.RotateTowards(transform.rotation, rocketTargetRotation, turn));
+    }
+}
