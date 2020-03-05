fb33448453538e2c050e01464d85a0e9be75796d
DanielPinnington <danielpinnington12@gmail.com>
2020-03-05 17:39:10 +0000

    Wrong Way Checking System
    If player has already been through a checkpoint and the player goes through the same one more than once, he's going the wrong way so display a message on screen.
    
    This will eventually be a Raycast, but for testing purposes this has been implemented for the time being.

--- Chainsaw Rider/Assets/Scripts/Checkpoints.cs
+++ Chainsaw Rider/Assets/Scripts/Checkpoints.cs
@@ -6,18 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class Checkpoints : MonoBehaviour
 {
+
+    public int countingLaps; //THIS WILL PROB NEED DELETING
     public Text checkPointPassed;
     public static bool checkpointbool1 = false;
     public static bool checkpointbool2 = false;
     public static bool checkpointbool3 = false;
     public static bool finish = false;
+    public static int countingWrongLaps;
     public static int lapCounter = 3;
     public static int lapCounting = 0;
+    public bool isLapStarted = false;
+
+    public Text lapTime;
+    public GameObject[] lapTimesArray;
 
     public GameObject canvas;
+    public GameObject RaceFinishCanvas;
     public bool pause;
 
-    [SerializeField] private string loadLevel;
+
+    // public float LastLapTime { get; private set; }
+    // public float BestLapTime { get; private set; }
+
+    float currentLapStartTime;
+
+    // [SerializeField] private string loadLevel;
 
     private float pauseUntil;
     private bool paused;
@@ -25,14 +39,17 @@ public class Checkpoints : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        checkpointbool1 = false;
+        checkpointbool2 = false;
+        checkpointbool3 = false;
+        finish = false;
+        lapCounting = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (paused) CheckPause();
+        // if (paused) CheckPause();
 
         if (finish == true)
         {
@@ -47,6 +64,10 @@ public class Checkpoints : MonoBehaviour
         {
             checkPointPassed.text = "Checkpoint 2 passed";
         }
+        else if (checkpointbool1 == true && countingWrongLaps >= 2)
+        {
+            checkPointPassed.text = "Wrong Way!!";
+        }
         else if (checkpointbool1 == true)
         {
             checkPointPassed.text = "Checkpoint 1 passed";
@@ -56,18 +77,39 @@ public class Checkpoints : MonoBehaviour
             checkPointPassed.text = "No checkpoints passed yet";
         }
     }
-    private void OnTriggerEnter(Collider other)
+
+
+    /*  public float CurrentLapTime
+      {
+          get
+          {
+              return Time.realtimeSinceStartup - currentLapStartTime;
+          }
+      }*/
+
+    private void OnTriggerExit(Collider other)
     {
         switch (other.tag)
         {
             case "Checkpoint1": //Checks Tag names Checkpoint1 
                 checkpointbool1 = true;
+
+                Debug.Log("Player goes through..");
+                countingWrongLaps = countingWrongLaps + 1;
+
+                if (countingWrongLaps >= 2)
+                {
+                    Debug.Log("Wrong Way");
+                }
                 break;
+
             case "Checkpoint2":  //Checks Tag names Checkpoint2
+
                 if (checkpointbool1 == true)
                 {
                     checkpointbool2 = true;
                 }
+
                 break;
             case "Checkpoint3":  //Checks Tag names Checkpoint3 
                 if (checkpointbool2 == true)
@@ -80,37 +122,66 @@ public class Checkpoints : MonoBehaviour
                 if (checkpointbool3 == true)
                 {
                     lapCounting = lapCounting + 1;
+                    // lapTimes();
+
                     if (checkpointbool1 == true && checkpointbool2 == true && checkpointbool3 == true && lapCounting < 3)
                     {
                         checkpointbool1 = false;
                         checkpointbool2 = false;
                         checkpointbool3 = false;
+                        countingWrongLaps = 0;
                     }
                     else
                     {
                         finish = true;
                         canvas.SetActive(true);
-                        PauseForDuration(1.0f);
+                        RaceFinishCanvas.SetActive(true);
                     }
                 }
                 break;
         }
     }
 
-    void PauseForDuration(float duration)
-    {
-        paused = true;
-        pauseUntil = Time.realtimeSinceStartup + duration;
-        Time.timeScale = 0.0f;
-    }
+    //   void PauseForDuration()
+    // {
+    //      paused = true;
+    //      pauseUntil = Time.realtimeSinceStartup + duration;
+    //      Time.timeScale = 0.0f;
+    //  }
 
-    void CheckPause()
-    {
-        if (Time.realtimeSinceStartup >= pauseUntil)
-        {
-            Time.timeScale = 1.0f;
-            paused = false;
-            SceneManager.LoadScene("YouWin");
-        }
-    }
+    //    void CheckPause()
+    //    {
+    //       if (Time.realtimeSinceStartup >= pauseUntil)
+    //     {
+    //         Time.timeScale = 1.0f;
+    //        paused = false;
+    //       SceneManager.LoadScene("YouWin");
+    //  }
+    // }
+
+    /*   void lapTimes()
+       {
+           LapTimeManager.MilliCount += Time.deltaTime * 1000;
+           LapTimeManager.MilliDisplay = LapTimeManager.MilliCount.ToString("F0");
+
+           string lapTime1 = "" + LapTimeManager.MinuteCount + ":" + LapTimeManager.SecondCount + "." + LapTimeManager.MilliCount.ToString("F0");
+
+           if (countingLaps == 0)
+           {
+               lapTimesArray[0].GetComponent<Text>().text = lapTime1;
+               countingLaps = countingLaps + 1;
+
+           }
+           else if(countingLaps == 1)
+           {
+               lapTimesArray[1].GetComponent<Text>().text = lapTime1;
+               countingLaps = countingLaps + 1;
+           }
+           else if (countingLaps == 2)
+           {
+               lapTimesArray[2].GetComponent<Text>().text = lapTime1;
+               countingLaps = countingLaps + 1;
+           }
+    */
 }
+
